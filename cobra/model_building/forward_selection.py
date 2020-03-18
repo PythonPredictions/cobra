f05f15479941b4f881d5b6fda81bbeb474b8b102
Matthias Roels <matthias.roels@pythonpredictions.com>
2020-03-18 15:32:20 +0100

    Add functions to explore result of forward_selection
    One function is to obtain the model of a particular step.
    The other function is to compute the performance (e.g. AUC) of the
    models in the different steps for train-selection-validation sets.
    The result of the later function is cached in each of the models to
    optimize recomputation.

--- cobra/model_building/forward_selection.py
+++ cobra/model_building/forward_selection.py
@@ -8,16 +8,19 @@ from cobra.model_building import LogisticRegressionModel as MLModel
 
 class ForwardFeatureSelection:
 
-    """Summary
+    """Perform forward feature selection for a given dataset using a given
+    model.
 
     Attributes
     ----------
     max_predictors : int
-        Description
+        maximum number of predictors allowed in any model. This corresponds
+        more or less with the maximum number of steps in the forward feature
+        selection
     model_name : str
-        Description
+        name of the model to use for forward feature selection
     pos_only : bool
-        Description
+        whether or not the model coefficients should all be positive
     """
 
     def __init__(self, max_predictors: int=50,
@@ -29,7 +32,90 @@ class ForwardFeatureSelection:
 
         self._fitted_models = []
 
-    def fit(self, data: pd.DataFrame, target_column_name: str,
+    def get_model_from_step(self, step: int) -> MLModel:
+        """Get fitted model from a particular step
+
+        Parameters
+        ----------
+        step : int
+            Particular step in the forward selection
+
+        Returns
+        -------
+        MLModel
+            Fitted model from the given step
+
+        Raises
+        ------
+        ValueError
+            in case step is larger than the number of available models
+        """
+        if len(self._fitted_models) < step:
+            raise ValueError(f"No model available for step {step}")
+
+        return self._fitted_models[step]
+
+    def compute_model_performances(self, data: pd.DataFrame,
+                                   target_column_name: str) -> list:
+        """Compute for each model the performance for
+           train-selection-validation sets and return them along with a list
+           of predictors used in the model.
+           Note that the computation of the performance for each split is
+           cached inside the model itself, so it is inexpensive to perform
+           it multiple times!
+
+        Parameters
+        ----------
+        data : pd.DataFrame
+            dataset for which to compute performance of each model
+        target_column_name : str
+            name of the target column
+
+        Returns
+        -------
+        list
+            A list containing for each model the performance for
+            train-selection-validation sets as well as the set of predictors
+            used in this model
+        """
+        results = []
+        predictor_set = set([])
+        for model in self._fitted_models:
+            # Evaluate model
+            performance_train = model.evaluate(
+                data[data["split"] == "train"],
+                data[data["split"] == "train"][target_column_name],
+                split="train"
+                )
+
+            performance_selection = model.evaluate(
+                data[data["split"] == "selection"],
+                data[data["split"] == "selection"][target_column_name],
+                split="selection"
+                )
+
+            performance_validation = model.evaluate(
+                data[data["split"] == "validation"],
+                data[data["split"] == "validation"][target_column_name],
+                split="validation"
+                )
+
+            last_added_predictor = (set(model.predictors)
+                                    .difference(predictor_set))
+
+            results.append({
+                "predictors": model.predictors,
+                "last_added_predictor": list(last_added_predictor)[0],
+                "train_performance": performance_train,
+                "selection_performance": performance_selection,
+                "validation_performance": performance_validation
+            })
+
+            predictor_set = predictor_set.union(set(model.predictors))
+
+        return results
+
+    def fit(self, train_data: pd.DataFrame, target_column_name: str,
             predictors: list, forced_predictors: list=[],
             excluded_predictors: list=[]):
         """Summary
@@ -67,21 +153,22 @@ class ForwardFeatureSelection:
                      "only one model will be trained...")
             # train model with all forced_predictors (only)
             (self._fitted_models
-             .append(self._train_model(data[data["split"] == "train"],
+             .append(self._train_model(train_data,
                                        target_column_name,
                                        forced_predictors)))
         else:
-            self._forward_selection(data, target_column_name,
+            self._forward_selection(train_data, target_column_name,
                                     filterd_predictors,
                                     forced_predictors)
 
-    def _forward_selection(self, data: pd.DataFrame, target_column_name: str,
-                           predictors: list, forced_predictors: list=[]):
+    def _forward_selection(self, train_data: pd.DataFrame,
+                           target_column_name: str, predictors: list,
+                           forced_predictors: list=[]):
         """Summary
 
         Parameters
         ----------
-        data : pd.DataFrame
+        train_data : pd.DataFrame
             Description
         target_column_name : str
             Description
@@ -92,7 +179,9 @@ class ForwardFeatureSelection:
         """
         current_predictors = []
 
-        for step in range(1, self.max_predictors + 1):
+        max_steps = 1 + min(self.max_predictors,
+                            len(predictors) + len(forced_predictors))
+        for step in range(1, max_steps):
             if step <= len(forced_predictors):
                 # first, we go through forced predictors
                 candidate_predictors = list(set(forced_predictors)
@@ -102,7 +191,7 @@ class ForwardFeatureSelection:
                 candidate_predictors = [var for var in predictors
                                         if var not in current_predictors]
 
-            model = self._find_next_best_model(data[data["split"] == "train"],
+            model = self._find_next_best_model(train_data,
                                                target_column_name,
                                                candidate_predictors,
                                                current_predictors)
@@ -114,12 +203,16 @@ class ForwardFeatureSelection:
                 break
 
             if model is not None:
+                # Add new model predictors to the list of current predictors
+                current_predictors = list(set(current_predictors)
+                                          .union(set(model.predictors)))
+
                 self._fitted_models.append(model)
 
         if not self._fitted_models:
             log.error("No models found in forward selection")
 
-    def _find_next_best_model(self, data: pd.DataFrame,
+    def _find_next_best_model(self, train_data: pd.DataFrame,
                               target_column_name: str,
                               candidate_predictors: list,
                               current_predictors: list) -> MLModel:
@@ -127,7 +220,7 @@ class ForwardFeatureSelection:
 
         Parameters
         ----------
-        data : pd.DataFrame
+        train_data : pd.DataFrame
             Description
         target_column_name : str
             Description
@@ -143,35 +236,36 @@ class ForwardFeatureSelection:
         """
         # placeholders
         best_model = None
-        best_auc = -1
+        best_performance = -1
 
         for pred in candidate_predictors:
 
             # train model with additional predictor
-            model = self._train_model(data, target_column_name,
+            model = self._train_model(train_data, target_column_name,
                                       (current_predictors + [pred]))
             # Evaluate model
-            auc_pred = model.evaluate(data[current_predictors + [pred]],
-                                      data[target_column_name])
+            performance = (model
+                           .evaluate(train_data[current_predictors + [pred]],
+                                     train_data[target_column_name]))
 
             if (self.pos_only and (not (model.get_coef() >= 0).all())):
                 continue
 
             # check if model is better than current best model
             # and if yes, replace current best!
-            if (auc_pred >= best_auc):
-                best_auc = auc_pred
+            if (performance >= best_performance):
+                best_performance = performance
                 best_model = model
 
         return best_model
 
-    def _train_model(self, data: pd.DataFrame, target_column_name: str,
+    def _train_model(self, train_data: pd.DataFrame, target_column_name: str,
                      predictors: list) -> MLModel:
         """Summary
 
         Parameters
         ----------
-        data : pd.DataFrame
+        train_data : pd.DataFrame
             Description
         target_column_name : str
             Description
@@ -185,6 +279,6 @@ class ForwardFeatureSelection:
         """
         model = MLModel()
 
-        model.fit(data[predictors], data[target_column_name])
+        model.fit(train_data[predictors], train_data[target_column_name])
 
         return model
