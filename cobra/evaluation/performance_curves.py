746c99b7fac3528b1d1adf4932f020248395d7b6
Matthias Roels <matthias.roels@pythonpredictions.com>
2020-03-25 14:14:09 +0100

    Change line endings to linux style in evaluation module

--- cobra/evaluation/performance_curves.py
+++ cobra/evaluation/performance_curves.py
@@ -5,26 +5,26 @@ import pandas as pd
 import matplotlib.pyplot as plt
 
 
-def plot_performance_curves(model_performances: list,
+def plot_performance_curves(model_performance: pd.DataFrame,
                             dim: tuple=(12, 8)):
 
-    df_plt = pd.DataFrame(model_performances)
-
-    highest_auc = np.round(max(max(df_plt['train_performance']),
-                               max(df_plt['selection_performance']),
-                               max(df_plt['validation_performance'])), 1)
+    highest_auc = np.round(max(max(model_performance['train_performance']),
+                               max(model_performance['selection_performance']),
+                               max(model_performance['validation_performance'])
+                               ), 1)
 
     fig, ax = plt.subplots(figsize=dim)
 
-    plt.plot(df_plt['train_performance'], marker=".", markersize=20,
+    plt.plot(model_performance['train_performance'], marker=".", markersize=20,
              linewidth=3, label='AUC train')
-    plt.plot(df_plt['selection_performance'], marker=".", markersize=20,
-             linewidth=3, label='AUC selection')
-    plt.plot(df_plt['validation_performance'], marker=".", markersize=20,
-             linewidth=3, label='AUC validation')
+    plt.plot(model_performance['selection_performance'], marker=".",
+             markersize=20, linewidth=3, label='AUC selection')
+    plt.plot(model_performance['validation_performance'], marker=".",
+             markersize=20, linewidth=3, label='AUC validation')
     # Set x/yticks
-    ax.set_xticks(np.arange(len(df_plt['last_added_predictor']) + 1))
-    ax.set_xticklabels(df_plt['last_added_predictor'].tolist(),
+    ax.set_xticks(np.arange(len(model_performance['last_added_predictor'])
+                            + 1))
+    ax.set_xticklabels(model_performance['last_added_predictor'].tolist(),
                        rotation=40, ha='right')
     ax.set_yticks(np.arange(0.5, highest_auc + 0.02, 0.05))
     #Make Pretty
