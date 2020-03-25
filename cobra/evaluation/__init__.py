ba01627a4081d42a0daa0cdab86708fca1bedbae
Matthias Roels <matthias.roels@pythonpredictions.com>
2020-03-25 11:22:31 +0100

    Modify evaluation's __init__.py

--- cobra/evaluation/__init__.py
+++ cobra/evaluation/__init__.py
@@ -1,5 +1,17 @@
-from .pigs_tables import generate_pig_tables, compute_pig_table, plot_pig_graph
+from .pigs_tables import generate_pig_tables
+from .pigs_tables import compute_pig_table
+from .pigs_tables import plot_pig_graph
+
+from .performance_curves import plot_performance_curves
+
+from .predictor_quality import plot_variable_importance
+from .predictor_quality import plot_predictor_quality
+from .predictor_quality import plot_correlation_matrix
 
 __all__ = ['generate_pig_tables',
            'compute_pig_table',
-           'plot_pig_graph']
+           'plot_pig_graph',
+           'plot_performance_curves',
+           'plot_variable_importance',
+           'plot_predictor_quality',
+           'plot_correlation_matrix']
