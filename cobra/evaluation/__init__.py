1b6a058e68fcad6647a25fe797907a4683bcc15d
Matthias Roels <matthias.roels@pythonpredictions.com>
2020-03-25 11:17:22 +0100

    Bug fix in evaluation __init__.py

--- cobra/evaluation/__init__.py
+++ cobra/evaluation/__init__.py
@@ -1,4 +1,4 @@
-from .pig_tables import generate_pig_tables, compute_pig_table, plot_pig_graph
+from .pigs_tables import generate_pig_tables, compute_pig_table, plot_pig_graph
 
 __all__ = ['generate_pig_tables',
            'compute_pig_table',
