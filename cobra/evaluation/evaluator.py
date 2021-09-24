09e51c601885133a52e28b4e297df4aa9b26cd3c
sborms <sam.borms@pythonpredictions.com>
2021-09-24 16:24:47 +0200

    fix qq plot

--- cobra/evaluation/evaluator.py
+++ cobra/evaluation/evaluator.py
@@ -682,8 +682,8 @@ class RegressionEvaluator():
             x = self.qq["quantiles"]
             y = self.qq["residuals"]
 
-            ax.plot(x, y, "o--.", color="cornflowerblue", linewidth=3)
-            ax.plot(x, x, "r--.", color="cornflowerblue", linewidth=3)
+            ax.plot(x, x, "r--", color="darkorange", linewidth=3)
+            ax.plot(x, y, "o--", color="cornflowerblue", linewidth=3)
 
             ax.set_xlabel("Theoretical quantiles", fontsize=15)
             ax.set_ylabel("Standardized residuals", fontsize=15)
