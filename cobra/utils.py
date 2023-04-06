d5ddbdfb337cc961d881d823a87df2a0973fe5f4
patrickleonardy <116005033+patrickleonardy@users.noreply.github.com>
2023-04-06 18:17:33 +0200

    Revert "#151 add turorial while importing"

--- cobra/utils.py
+++ cobra/utils.py
@@ -1,24 +1,7 @@
-import logging
-
-# logger = logging.getLogger(__name__)
-# logger.setLevel(logging.INFO)
-# logger.addHandler(logging.Handler())
-
-
 def clean_predictor_name(predictor_name: str) -> str:
     """Strip the redundant suffix (e.g. "_enc" or "_bin") off from the end
     of the predictor name to return a clean version of the predictor
     """
-    return (
-        predictor_name.replace("_enc", "").replace("_bin", "").replace("_processed", "")
-    )
-
-
-def log_tutorial() -> None:
-    logging.info(
-        """
-    Hi, welcome to Cobra!
-    You can find some tutorials that explain the functioning of cobra on the PythonPredictions GitHub:
-    https://github.com/PythonPredictions/cobra/tree/master/tutorials
-        """
-    )
+    return (predictor_name.replace("_enc", "")
+                          .replace("_bin", "")
+                          .replace("_processed", ""))
