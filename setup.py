294ac996c91e5a2f18b41ca48302994902a032e1
Sander Vanden Hautte <sander.vandenhautte@tobania.be>
2023-04-07 11:26:23 +0200

    Enforcing pandas<2.0 in both requirements.txt and setup.py.

--- setup.py
+++ setup.py
@@ -13,8 +13,9 @@ README = (ROOT / "README.rst").read_text()
 setup(
     name="pythonpredictions-cobra",
     version=__version__,
-    description=("A Python package to build predictive linear and logistic regression "
-                 "models focused on performance and interpretation."),
+    description=("A Python package to build predictive linear and logistic "
+                 "regression models focused on performance and "
+                 "interpretation."),
     long_description=README,
     long_description_content_type="text/x-rst",
     packages=find_packages(include=["cobra", "cobra.*"]),
@@ -24,7 +25,7 @@ setup(
     author_email="cobra@pythonpredictions.com",
     install_requires=[
         "numpy>=1.19.4",
-        "pandas>=1.1.5",
+        "pandas>=1.1.5,<2.0",
         "scipy>=1.5.4",
         "scikit-learn>=0.24.1",
         "matplotlib>=3.4.3",
