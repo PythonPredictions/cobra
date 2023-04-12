5e4109aa16a18132454e3b115d180b47450b9d63
Pietro D'Antuono <pietro.dantuono@vub.be>
2023-04-12 16:05:27 +0200

    fix: dev: Fixed inv test command on Windows machines.

--- tasks/test.py
+++ tasks/test.py
@@ -6,12 +6,18 @@ from .system import (
     OperatingSystem,
     get_current_system,
     COV_DOC_BUILD_DIR,
-    COV_SCREEN_NAME,
 )
+
+SYSTEM = get_current_system()
+
+PTY = False
+if SYSTEM in [OperatingSystem.LINUX, OperatingSystem.MAC]:
+    from .system import COV_SCREEN_NAME
+    PTY = True
 
 
 @task(help={"verbose": "Run tests verbose."})
-def run(c_r, verbose=False):
+def run(c_r, verbose=True):
     """Run test suite."""
     # print('I am run(c) and I was called!')
     # print('I am c!\n', c_r, '\n', type(c), '\n', c_r.__dict__)  # <-- c_r?
@@ -20,14 +26,14 @@ def run(c_r, verbose=False):
             f"pytest -v -W ignore::UserWarning \
             --cov={c_r.project_slug} --cov-report=term:skip-covered \
             --cov-report=html --cov-report=html:{COV_DOC_BUILD_DIR}",
-            pty=True,
+            pty=PTY,
         )
     else:
         c_r.run(
             f"pytest -W ignore::UserWarning \
             --cov={c_r.project_slug} --cov-report=term:skip-covered \
             --cov-report=html --cov-report=html:{COV_DOC_BUILD_DIR}",
-            pty=True,
+            pty=PTY,
         )
 
 
@@ -35,16 +41,15 @@ def run(c_r, verbose=False):
 def coverage(c_r):
     """Start coverage report webserver."""
     COV_PORT = c_r.start_port + 2
-    system = get_current_system()
 
-    if system in [OperatingSystem.LINUX, OperatingSystem.MAC]:
+    if SYSTEM in [OperatingSystem.LINUX, OperatingSystem.MAC]:
         _command = f"screen -d -S {COV_SCREEN_NAME} \
             -m python -m http.server --bind localhost \
             --directory {COV_DOC_BUILD_DIR} {COV_PORT}"
-    elif system == OperatingSystem.WINDOWS:
+    elif SYSTEM == OperatingSystem.WINDOWS:
         _command = f"wt -d . python -m http.server --bind localhost --directory {COV_DOC_BUILD_DIR} {COV_PORT}"  # noqa: E501 # pylint: disable=line-too-long
     else:
-        raise ValueError(f"System {system} is not supported")
+        raise ValueError(f"System {SYSTEM} is not supported")
 
     c_r.run(_command)
 
@@ -58,9 +63,8 @@ def coverage(c_r):
 @task
 def stop(c_r):
     """Stop coverage report webserver."""
-    system = get_current_system()
 
-    if system == OperatingSystem.LINUX or system == OperatingSystem.MAC:
+    if SYSTEM in [OperatingSystem.LINUX, OperatingSystem.MAC]:
         result = c_r.run(
             f"screen -ls {COV_SCREEN_NAME}", warn=True, hide="both"
         )
@@ -70,14 +74,14 @@ def stop(c_r):
         for scr in screens:
             name = scr.split("\t")[1]
             c_r.run(f"screen -S {name} -X quit")
-    elif system == OperatingSystem.WINDOWS:
+    elif SYSTEM == OperatingSystem.WINDOWS:
         print(
-            "Coverage server is not attached to this process."
+            "Coverage server is not attached to this process. "
             "Close windows terminal instance instead"
         )
         return
-    # else:
-        # raise ValueError(f"System {system} is not supported")
+    else:
+        raise ValueError(f"System {SYSTEM} is not supported")
 
 
 @task(post=[stop, run, coverage], default=True)
