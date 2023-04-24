6dcff4806e134917dd12073a78babf3f08de4d02
patrickleonardy <patric.leonardy@pythonpredictions.com>
2023-04-24 13:31:08 +0200

    undo the hello message while importing

--- cobra/__init__.py
+++ cobra/__init__.py
@@ -1,7 +1,1 @@
 from .version import __version__
-from cobra.utils import log_tutorial
-import logging
-
-logging.basicConfig(level=logging.INFO, format="%(message)s")
-
-log_tutorial()
--- cobra/utils.py
+++ cobra/utils.py
@@ -1,10 +1,3 @@
-import logging
-
-# logger = logging.getLogger(__name__)
-# logger.setLevel(logging.INFO)
-# logger.addHandler(logging.Handler())
-
-
 def clean_predictor_name(predictor_name: str) -> str:
     """Strip the redundant suffix (e.g. "_enc" or "_bin") off from the end
     of the predictor name to return a clean version of the predictor
@@ -14,11 +7,3 @@ def clean_predictor_name(predictor_name: str) -> str:
     )
 
 
-def log_tutorial() -> None:
-    logging.info(
-        """
-    Hi, welcome to Cobra!
-    You can find some tutorials that explain the functioning of cobra on the PythonPredictions GitHub:
-    https://github.com/PythonPredictions/cobra/tree/master/tutorials
-        """
-    )
