eb20efe37aa282ba60df4c6240b4af13843c52ff
patrickleonardy <patric.leonardy@pythonpredictions.com>
2023-04-24 13:31:48 +0200

    introduce text for the help function in init

--- cobra/__init__.py
+++ cobra/__init__.py
@@ -1,1 +1,19 @@
+"""
+
+Cobra is a Python package to build predictive models using linear or logistic regression with a focus on performance and interpretation. 
+It consists of several modules for data preprocessing, feature selection and model evaluation. 
+The underlying methodology was developed at Python Predictions in the course of hundreds of business-related prediction challenges. 
+It has been tweaked, tested and optimized over the years based on feedback from clients, our team, and academic researchers.
+
+
+You can find some tutorials that explain the functioning of cobra on the PythonPredictions GitHub:
+https://github.com/PythonPredictions/cobra/tree/master/tutorials
+
+The documentation:
+https://pythonpredictions.github.io/cobra.io/
+
+Presentation about the inner workings of Cobra:
+https://github.com/PythonPredictions/cobra/blob/develop/Cobra.pdf
+"""
+
 from .version import __version__
